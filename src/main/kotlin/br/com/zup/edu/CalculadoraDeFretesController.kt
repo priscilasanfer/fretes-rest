27d3e123d9ec93f4381ed1131853e68b07a53aff
Priscila Ferreira <priscilasanfer@gmail.com>
2021-06-02 19:53:43 -0300

    Lidando com erros retornados pela API gRPC

--- src/main/kotlin/br/com/zup/edu/CalculadoraDeFretesController.kt
+++ src/main/kotlin/br/com/zup/edu/CalculadoraDeFretesController.kt
@@ -1,8 +1,14 @@
 package br.com.zup.edu
 
+import com.google.protobuf.Any
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 import javax.inject.Inject
 
 @Controller
@@ -16,12 +22,37 @@ class CalculadoraDeFretesController(
             .setCep(cep)
             .build()
 
-        val response = grpcClient.calculaFrete(request)
+        try {
+            val response = grpcClient.calculaFrete(request)
 
-        return FreteResponse(
-            cep = response.cep,
-            valor = response.valor
-        )
+            return FreteResponse(
+                cep = response.cep,
+                valor = response.valor
+            )
+        } catch (e: StatusRuntimeException) {
+
+            val description = e.status.description
+            val statusCode = e.status.code
+
+            if (statusCode == Status.Code.INVALID_ARGUMENT) {
+                throw  HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+
+            if (statusCode == Status.Code.PERMISSION_DENIED) {
+                val statusProto = StatusProto.fromThrowable(e)
+
+                if (statusProto == null) {
+                    throw HttpStatusException(HttpStatus.FORBIDDEN, description)
+                }
+
+                val anyDetials: Any = statusProto.detailsList.get(0)
+                val errorDetails = anyDetials.unpack(ErrorDetials::class.java)
+
+                throw HttpStatusException(HttpStatus.FORBIDDEN, "${errorDetails.code}: ${errorDetails.message }")
+            }
+
+            throw  HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        }
     }
 }
 
